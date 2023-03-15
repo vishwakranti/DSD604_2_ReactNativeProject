710301e867c1d1e945f38058f54fa2aa35b36b38
Vishwakranti Suryawanshi/Musalgavkar <87359394+vishwakranti@users.noreply.github.com>
2023-03-15 15:17:18 +1300

    updated app.js

--- App.js
+++ App.js
@@ -4,11 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test your city knowledge!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+
 
 const styles = StyleSheet.create({
   container: {
