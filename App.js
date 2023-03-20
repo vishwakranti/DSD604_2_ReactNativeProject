e15f28aa550eab809141a6c014e34e6bf37a62fc
vishwakranti <vishwakrantisuryawanshi@gmail.com>
2023-03-21 01:15:08 +1300

    added bottom navigator tab to home screen

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const BottomTab = createBottomTabNavigator();
+
+function HomeScreen() {
+  return (
+  <View style={styles.container}>
+    <Text style={styles.text}>Test your city knowledge!</Text>
+    <StatusBar style="auto" />
+  </View>);
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Test your city knowledge!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <BottomTab.Navigator>
+        <BottomTab.Screen name="Home" component={HomeScreen} />
+        {/* <BottomTab.Screen name="Settings" component={SettingsScreen} /> */}
+      </BottomTab.Navigator>
+    </NavigationContainer>
+  )
+
+  };
+
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0000ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color: '#ffffff',
+    fontSize: 25,
+  }
 });
